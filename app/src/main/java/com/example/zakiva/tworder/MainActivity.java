217ec62c2aaba47577d2a22f31cd1cf9e79cb00c
Zahi Akiva <zakiva@redhat.com>
2015-11-13 15:04:44 +0200

    Add Parse Dependencies

--- app/src/main/java/com/example/zakiva/tworder/MainActivity.java
+++ app/src/main/java/com/example/zakiva/tworder/MainActivity.java
@@ -5,12 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static boolean parse_init = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (parse_init){
+            // Enable Local Datastore.
+            Parse.enableLocalDatastore(this);
+            Parse.initialize(this, "JwL3fVDM1GoYSss63FFTwy7jyLt1abq7gYMfiDij", "MRKSsUUKU0AMGRYNTiEz48DXJ4KovXAaqHz0UAG5");
+            parse_init = false;
+        }
     }
 
     public void onNextClick(View view){
