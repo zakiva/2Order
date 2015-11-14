f6dfb8641fcdfa400d5ce75224890d18f9e08188
Ariel Bl <Ariel Bl>
2015-11-14 21:12:17 +0200

    sign up customer

--- app/src/main/java/com/example/zakiva/tworder/logIn_screen.java
+++ app/src/main/java/com/example/zakiva/tworder/logIn_screen.java
@@ -1,10 +1,40 @@
 package com.example.zakiva.tworder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class logIn_screen extends AppCompatActivity {
+
+    void sign_in(String username, String password)
+    {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    String s = (String) ParseUser.getCurrentUser().get("kind");
+                    if (s.equals("business")) {
+                        //what happens if sign in succeeded for business user
+                        Intent intent = new Intent(getBaseContext(), business_orders__screen.class);
+                        startActivity(intent);
+                    } else {
+                        //what happens if sign in succeeded for customer user
+                        //Intent intent = new Intent(getBaseContext(), Main22Activity.class);
+                        //startActivity(intent);
+                    }
+                } else {
+                    //what happens if sign in failed
+                    //TextView textView3 = (TextView) findViewById(R.id.textView3);
+                    //textView3.setText("Failed!!!");
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/zakiva/tworder/new_business_screen.java
+++ app/src/main/java/com/example/zakiva/tworder/new_business_screen.java
@@ -6,8 +6,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 public class new_business_screen extends AppCompatActivity {
 
+    void sign_up(String username, String password, String name)
+    {
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.put("kind", "business");
+        user.put("name", name);
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null)
+                {
+                    Intent intent = new Intent(getBaseContext(), business_orders__screen.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    //what happens if sign up failed
+                    //TextView textView3 = (TextView) findViewById(R.id.textView3);
+                    //textView3.setText("Failed!!!");
+                }
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +45,7 @@ public class new_business_screen extends AppCompatActivity {
 
         // GO BACK TO THE SAME ACTIVITY
 
-        Intent i = new Intent(this, business_orders__screen.class);
+        //Intent i = new Intent(this, business_orders__screen.class);
         final EditText userNameInput = (EditText) findViewById(R.id.usernameInput);
         final EditText businessNameInput = (EditText) findViewById(R.id.businessNameInput);
         final EditText passInput = (EditText) findViewById(R.id.passwordInput);
@@ -26,15 +53,17 @@ public class new_business_screen extends AppCompatActivity {
         String name = userNameInput.getText().toString();
         String pass = passInput.getText().toString();
         String busiName = businessNameInput.getText().toString();
+
+        sign_up(name, pass, busiName);
         //String busiAdd =  businessAdressInput.getText().toString();
-        i.putExtra("userName", name);
-        i.putExtra("password", pass);
-        i.putExtra("businessName", busiName);
+        //i.putExtra("userName", name);
+        //i.putExtra("password", pass);
+        //i.putExtra("businessName", busiName);
         //i.putExtra("businessAdress", busiAdd);
 
         // need to check the validity of the password
 
-        startActivity(i);
+        //startActivity(i);
     }
 
 }
