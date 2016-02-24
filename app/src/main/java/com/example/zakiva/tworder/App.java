4f228d98f5f3f93b8d3513e14a7b166ce69062a4
Ariel Bl <Ariel Bl>
2016-02-25 01:54:48 +0200

    RTL languages fix

--- app/src/main/java/com/example/zakiva/tworder/App.java
+++ app/src/main/java/com/example/zakiva/tworder/App.java
@@ -2,15 +2,27 @@ package com.example.zakiva.tworder; /**
  * Created by Ariel on 11/18/2015.
  */
 import android.app.Application;
+import android.content.res.Configuration;
+
 import com.facebook.FacebookSdk;
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseInstallation;
 
+import java.util.Locale;
+
 public class App extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
+
+        Locale locale = new Locale("en_US");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "JwL3fVDM1GoYSss63FFTwy7jyLt1abq7gYMfiDij", "MRKSsUUKU0AMGRYNTiEz48DXJ4KovXAaqHz0UAG5");
         FacebookSdk.sdkInitialize(getApplicationContext());
